8b027a2c03eb7433312e624b29d328d6396b030a
qienzgao <qienzgao@gmail.com>
2021-12-17 12:19:50 -0500

    1st swap

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -1,3 +1,20 @@
+require "byebug"
 class PolyTreeNode
+    attr_reader :children, :parent, :value
+    def initialize(value)
+        @value = value
+        @parent = nil
+        @children = []
+    end 
 
+    def parent=(node)
+        @parent = node
+        if @parent != nil && !@parent.children.include?(self)
+            @parent.children << self 
+        end
+    end
+
+
+
+    
 end

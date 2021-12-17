89027798614961b34bec57c64dbaffa4f4c34b3d
qienzgao <qienzgao@gmail.com>
2021-12-17 14:57:17 -0500

    final

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -26,8 +26,31 @@ class PolyTreeNode
         else
             raise "Error: not a child"
         end
-
-
     end
+
+    def dfs(target)
+        return self if self.value == target
+        self.children.each do |child|
+            search_result = child.dfs(target)
+            return search_result if search_result != nil
+        end
+        nil 
+    end 
     
+    def bfs(target)
+        arr = [self]
+        until arr.empty?
+            node = arr.shift
+            return node if node.value == target
+            arr += node.children
+        end 
+        nil 
+    end
 end
+
+    
+
+
+
+
+# end 
